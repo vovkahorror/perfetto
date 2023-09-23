b6138602fa9ffd042f358bf19755d73a2f850b26
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-09-24 00:13:48 +0300

    feat: updated drinks

--- src/store/drinks.ts
+++ src/store/drinks.ts
@@ -12,8 +12,29 @@ export const CAFFELATTE = 'Caffelatte';
 export const FLAT_WHITE = 'Flat White';
 export const ESPRESSO_MACCHIATO = 'Espresso Macchiato';
 export const CHOCOLATE_MIX_CARAFE = 'Chocolate (Mix Carafe)';
-export const COLD_COFFEE_MIX_CARAFE = 'Cold Coffee (Mix Carafe)'
+export const COLD_COFFEE_MIX_CARAFE = 'Cold Coffee (Mix Carafe)';
 export const COLD_MILK_FOAM_MIX_CARAFE = 'Cold milk foam (Mix Carafe)';
 export const TEA_FUNCTION = 'Tea Function';
 export const HOT_WATER = 'Hot Water';
 export const STEAM = 'Steam';
+
+export type DrinksType = Array<
+    typeof ESPRESSO
+    | typeof COFFEE
+    | typeof LONG
+    | typeof DOPPIO_PLUS
+    | typeof AMERICANO
+    | typeof CAPPUCCINO
+    | typeof LATTE_MACCHIATO
+    | typeof CAPPUCCINO_PLUS
+    | typeof CAPPUCCINO_MIX
+    | typeof HOT_MILK
+    | typeof CAFFELATTE
+    | typeof FLAT_WHITE
+    | typeof ESPRESSO_MACCHIATO
+    | typeof CHOCOLATE_MIX_CARAFE
+    | typeof COLD_COFFEE_MIX_CARAFE
+    | typeof COLD_MILK_FOAM_MIX_CARAFE
+    | typeof TEA_FUNCTION
+    | typeof HOT_WATER
+    | typeof STEAM>;
