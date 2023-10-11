4ed59089df355c8a0b09a57aabe9fe2f8242de72
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-10-12 00:20:40 +0300

    feat: updated styles

--- src/screens/models/model/model.tsx
+++ src/screens/models/model/model.tsx
@@ -3,17 +3,35 @@ import {CoffeeMachine} from '../../../store/models';
 
 export const Model: ListRenderItem<CoffeeMachine> = ({item}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Image style={styles.image} source={{uri: item.imageUrl[0]}}/>
-            <Text>{item.model}</Text>
-            <Text>{item.series}</Text>
+            <View>
+                <Text style={styles.model}>{item.model}</Text>
+                <Text style={styles.series}>{item.series}</Text>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        marginVertical: 10,
+    },
     image: {
-        width: 300,
-        height: 300,
+        width: 150,
+        height: 150,
+        resizeMode: 'cover',
+        borderRadius: 10,
+    },
+    model: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    series: {
+        fontSize: 16,
+        fontStyle: 'italic',
     },
 });
--- src/screens/models/models.tsx
+++ src/screens/models/models.tsx
@@ -1,22 +1,28 @@
-import {FlatList, View} from 'react-native';
+import {FlatList, View, StyleSheet, ImageBackground} from 'react-native';
 import {Model} from './model/model';
 import {useAppSelector} from '../../utils/custom-hooks/useAppSelector';
 import {selectModels} from './selectors';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
-export const Models = ({}: ModelsProps) => {
+export const Models = () => {
     const insets = useSafeAreaInsets();
     const models = useAppSelector(selectModels);
 
     return (
         <View style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
-            <FlatList
-                data={models}
-                renderItem={Model}/>
+            <ImageBackground source={require('../../../assets/background/background1.webp')}>
+                <FlatList
+                    data={models}
+                    renderItem={Model}
+                    contentContainerStyle={{paddingHorizontal: 20}}/>
+            </ImageBackground>
         </View>
     );
 };
 
-interface ModelsProps {
-
-}
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        resizeMode: 'contain',
+    }
+})
