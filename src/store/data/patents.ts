348f3ac35aa786e45b86fe7ef2115b64b7002814
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-11-19 00:57:39 +0200

    feat: updated patents

--- src/store/data/patents.ts
+++ src/store/data/patents.ts
@@ -11,4 +11,5 @@ export class Patent {
 export const crf = new Patent(
     'https://www.dropbox.com/scl/fi/5acmyrayfl38ya2jgf5zo/CRF.jpeg?rlkey=cfsrn6icrskynwsvseeyvo3hl&dl=0&raw=1',
     'CRF',
-    '')
+    'CRF (Compact - Компактність, Reliable - Надійність, Fresh - Свіжість) — це комплекс взаємодії системи Tubeless та заварювального блоку. В основу системи закладено три принципи: легко виймається; не вимагає спеціальних знань для здійснення догляду; легко промивається водою. Це гарантує споживачу економію часу та коштів.');
+
