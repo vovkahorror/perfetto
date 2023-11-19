805e0a406fa062041fd184d4436916306342edd6
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-11-20 01:29:20 +0200

    feat: updated patents

--- src/store/data/patents.ts
+++ src/store/data/patents.ts
@@ -9,7 +9,12 @@ export class Patent {
 }
 
 export const crf = new Patent(
-    'https://www.dropbox.com/scl/fi/5acmyrayfl38ya2jgf5zo/CRF.jpeg?rlkey=cfsrn6icrskynwsvseeyvo3hl&dl=0&raw=1',
+    'https://www.dropbox.com/scl/fi/pdbeirc2n4jcg0001oc0e/CRF.jpg?rlkey=l4onir9xwfy817p912r6rcf7n&dl=0&raw=1',
     'CRF',
     'CRF (Compact - Компактність, Reliable - Надійність, Fresh - Свіжість) — це комплекс взаємодії системи Tubeless та заварювального блоку. В основу системи закладено три принципи: легко виймається; не вимагає спеціальних знань для здійснення догляду; легко промивається водою. Це гарантує споживачу економію часу та коштів.');
+
+export const tubeless = new Patent(
+    'https://www.dropbox.com/scl/fi/v2dutt7j5cn7nxs5r43ux/Tubeless.jpg?rlkey=oyonu2z2xczh5e16f9jm9rlt8&dl=0&raw=1',
+    'Tubeless',
+    'Заварювальний блок кавомашин DeLonghi під’їжджає впритул до кавомолки в момент її роботи. Мелена кава подається напряму в заварювальний блок без трубок та воронок. Завдяки цьому мінімізується кількість кави, яка залишається в системі після кожного приготування. Тому споживач завжди п’є каву, яка була щойно змелена, іншими словами - бездоганно-свіжу каву.');
 
