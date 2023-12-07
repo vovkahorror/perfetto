273ad19195d305653199f2c8615c67562ab6f563
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-12-08 00:34:29 +0200

    feat: added flatlist

--- src/screens/TechnologiesTab/TechnologiesTab.tsx
+++ src/screens/TechnologiesTab/TechnologiesTab.tsx
@@ -1,14 +1,32 @@
-import {Text} from 'react-native';
+import {FlatList, ListRenderItem, StyleSheet} from 'react-native';
 import {Wrapper} from '../../utils/hoc/Wrapper';
 import {useAppSelector} from '../../utils/custom-hooks/useAppSelector';
 import {selectTechnologies} from './selectors';
+import {TechnologyItem} from './TechnologyItem/TechnologyItem';
+import {Technology} from '../../store/data/technologies';
+import {PADDING_HORIZONTAL, PADDING_VERTICAL} from '../../constants/constants';
 
 export const TechnologiesTab = () => {
     const technologies = useAppSelector(selectTechnologies);
 
+    const renderItem: ListRenderItem<Technology> = ({item}) => (
+        <TechnologyItem item={item}/>);
+
     return (
         <Wrapper>
-            <Text>Технології</Text>
+            <FlatList
+                data={technologies}
+                renderItem={renderItem}
+                contentContainerStyle={styles.contentContainerStyle}
+                keyboardShouldPersistTaps={'always'}/>
         </Wrapper>
     );
 };
+
+const styles = StyleSheet.create({
+    contentContainerStyle: {
+        paddingHorizontal: PADDING_HORIZONTAL,
+        paddingVertical: PADDING_VERTICAL,
+        gap: 10,
+    },
+});
