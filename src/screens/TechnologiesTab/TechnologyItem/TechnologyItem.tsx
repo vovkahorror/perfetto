25bc10816135d4b1a28f3b85c53993e0cfda92bf
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-12-10 01:17:40 +0200

    refactor: updated TechnologyItem screen

--- src/screens/TechnologiesTab/TechnologyItem/TechnologyItem.tsx
+++ src/screens/TechnologiesTab/TechnologyItem/TechnologyItem.tsx
@@ -1,10 +1,17 @@
-import {Text, View} from 'react-native';
+import {Image, Text, StyleSheet, Dimensions, View} from 'react-native';
 import {Technology} from '../../../store/data/technologies';
+import {ListAccordion} from '../../../utils/hoc/ListAccordion';
+import {BACKGROUND_COLOR, BORDER_RADIUS, GAP, PADDING_HORIZONTAL} from '../../../constants/constants';
 
 export const TechnologyItem = ({item}: TechnologyItemProps) => {
     return (
-        <View>
-            <Text>TechnologyItem</Text>
+        <View style={styles.container}>
+            <ListAccordion title={item.title}>
+                <View style={styles.contentContainer}>
+                    <Image source={{uri: item.image}} style={styles.image} resizeMode={'cover'}/>
+                    <Text style={styles.text}>{item.description}</Text>
+                </View>
+            </ListAccordion>
         </View>
     );
 };
@@ -12,3 +19,25 @@ export const TechnologyItem = ({item}: TechnologyItemProps) => {
 interface TechnologyItemProps {
     item: Technology;
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: BACKGROUND_COLOR,
+        borderRadius: BORDER_RADIUS,
+    },
+    contentContainer: {
+        paddingLeft: PADDING_HORIZONTAL,
+        paddingRight: PADDING_HORIZONTAL,
+        gap: GAP,
+    },
+    image: {
+        alignSelf: 'center',
+        width: Dimensions.get('window').width - PADDING_HORIZONTAL * 4,
+        height: 300,
+        borderRadius: BORDER_RADIUS,
+    },
+    text: {
+        paddingLeft: PADDING_HORIZONTAL,
+        paddingRight: PADDING_HORIZONTAL,
+    }
+});
