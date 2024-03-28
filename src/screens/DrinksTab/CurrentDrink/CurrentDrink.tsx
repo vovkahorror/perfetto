fd8c3c0905bdfd4ddcb9b8fcfa524e5023089919
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-03-28 03:26:43 +0200

    feat: updated CurrentDrink screen

--- src/screens/DrinksTab/CurrentDrink/CurrentDrink.tsx
+++ src/screens/DrinksTab/CurrentDrink/CurrentDrink.tsx
@@ -1,4 +1,4 @@
-import {Dimensions, ScrollView, StyleSheet, Text} from 'react-native';
+import {Dimensions, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {Wrapper} from '../../../utils/components/Wrapper';
 import {
     BACKGROUND_COLOR,
@@ -25,23 +25,47 @@ export const CurrentDrink = ({route}: CurrentDrinkProps) => {
 
     return (
         <Wrapper>
-            <ScrollView contentContainerStyle={styles.container}>
-                <Text style={styles.name}>{name}</Text>
-                <ImageWithLoading styles={styles.image} source={{uri: imageUrl}}/>
-                <Text style={styles.description}>{description}</Text>
+            <View style={styles.container}>
+                <ScrollView>
+                    <Text style={styles.name}>{name}</Text>
+                    <ImageWithLoading styles={styles.image} source={{uri: imageUrl}}/>
+                    <Text style={styles.description}>{description}</Text>
 
-                <ListAccordion
-                    title="Процес приготування"
-                    icon="eyedropper">
-                    {data.process.beanWeight &&
-                        <ListItem title="Вага кавових зерен, г" description={data.process.beanWeight}/>}
-                    {data.process.preWetting !== null && data.process.preWetting !== undefined &&
-                        <ListItem title="Попереднє змочування" description={data.process.preWetting ? 'Так' : 'Ні'}/>}
-                    {data.process.pressure &&
-                        <ListItem title="Тиск при заварюванні" description={data.process.pressure}/>}
-                </ListAccordion>
-            </ScrollView>
+                    <ListAccordion
+                        title="Процес приготування"
+                        icon="eyedropper">
+                        <ListItemWithCheck title="Вага кавових зерен, г" description={data.process.beanWeight}/>
+                        <ListItemWithCheck title="Попереднє змочування" description={data.process.preWetting}/>
+                        <ListItemWithCheck title="Тиск при заварюванні" description={data.process.pressure}/>
+                        <ListItemWithCheck title="Об'єм кави" description={data.process.coffee}/>
+                        <ListItemWithCheck title="Об'єм молока" description={data.process.milk}/>
+                        <ListItemWithCheck title="Молочна пінка" description={data.process.milkFoam}/>
+                        <ListItemWithCheck title="Порядок подачі" description={data.process.submission}/>
+                    </ListAccordion>
+
+                    <ListAccordion
+                        title="Результат"
+                        icon="coffee-to-go">
+                        <ListItemWithCheck title="Об'єм" description={data.result.volume}/>
+                        <ListItemWithCheck title="Зовнішній вигляд" description={data.result.appearance}/>
+                    </ListAccordion>
+                </ScrollView>
+            </View>
         </Wrapper>
+    );
+};
+
+const ListItemWithCheck = ({title, description, ...props}: {
+    title: string,
+    description?: string | number | boolean
+}) => {
+    const simplifiedDescription = typeof description === 'boolean' ? (description ? 'Так' : 'Ні') : description;
+
+    return (
+        <>
+            {description !== null && description !== undefined &&
+                <ListItem title={title} description={simplifiedDescription} {...props}/>}
+        </>
     );
 };
 
